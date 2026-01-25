f148ee59f7e01b28f57c74c2288c768460a7453f
Daniel Roja <daniel.red.jb@gmail.com>
2026-01-25 13:39:35 -0600

    feat: ✨agregue el componente ArrowDownBoldIcon

---
+++ components/icons/ArrowDownBold.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+}
+
+export const ArrowDownBoldIcon = ({
+  size = 28,
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      className={className}
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m12.37 15.835l6.43-6.63C19.201 8.79 18.958 8 18.43 8H5.57c-.528 0-.771.79-.37 1.205l6.43 6.63c.213.22.527.22.74 0"
+      />
+    </svg>
+  );
+};
