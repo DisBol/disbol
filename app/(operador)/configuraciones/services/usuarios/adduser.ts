93092e055c116c269f0e5b46d19e001355878ee2
Johnn Hidalgo <johnn.hidalgo@inti.com.bo>
2026-05-01 14:02:39 -0400

    create user after client

--- app/(operador)/configuraciones/services/usuarios/adduser.ts
+++ app/(operador)/configuraciones/services/usuarios/adduser.ts
@@ -5,13 +5,15 @@ export async function AddUser(
   username: string,
   password: string,
   Role_id: string,
+  Client_id: number = 1,
+  Employee_id: number = 1,
 ): Promise<AddUserResponse> {
   return apiCall("adduser", {
     username: username,
     password: password,
     active: "true",
     Role_id: Role_id,
-    Client_id: "1",
-    Employee_id: "1",
+    Client_id,
+    Employee_id,
   });
 }
